9a470f2fe3ae33c91aaf24f975a9532fba872647
Robert Epperson <robbieepperson@yahoo.com>
2022-11-03 15:08:12 -0600

    commit

--- server/server.js
+++ server/server.js
@@ -8,6 +8,19 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+
+})
+
+console.log('server running')
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const {home, css, button} = require("./controllers/pageCtrl")
 
 app.get("/", home)
